95ca08def613d081bee34ea09836c504fbad34bb
Poramee Promkeeree <poramee0013@gmail.com>
2023-02-17 22:03:36 +0700

    add anouncement page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,14 @@ import Login from './page/login'
 import Main from './page/main'
 import Logout from './page/Logout'
 import Register from './page/Register'
-import { Announcement } from '@mui/icons-material'
+import Announcement from './page/Announcement'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Announcement/>} />
-        <Route path='/Home' errorElement={<Home/>}/>
+        <Route path="/Home" element={<Home />}/>
         <Route path="/index" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={ <Main />} />
--- client/src/page/Announcement.tsx
+++ client/src/page/Announcement.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider ,ThemeOptions } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#F5FFFA', // set to light blue color
+    },
+  },
+});
 
 const Announcement = () => {
   return (
-    <div>Announcement</div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500 }}>
+        <Box sx={{ margin: 'auto' }}>
+          <img src="/anm.png" width='841' height='410'/>
+        </Box>
+      </Grid>
+      <Grid item>
+        <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Link href="Home" variant="body2">
+            <Button type="submit" variant="contained" sx={{ mr: 1 }}>
+              เข้าสู่เว็ปไซต์
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   )
 }
 
