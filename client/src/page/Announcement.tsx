975c3b3c3fe6e61db9c5b3bd02b31a570d575504
นิธิกร ปานสังข์ <pansang1223@gmail.com>
2023-02-21 20:44:23 +0700

    fix Announcement

--- client/src/page/Announcement.tsx
+++ client/src/page/Announcement.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider ,ThemeOptions } from '@mui/material/styles';
 
 const theme = createTheme({
@@ -23,13 +19,13 @@ const Announcement = () => {
       <CssBaseline />
       <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500 }}>
         <Box sx={{ margin: 'auto' }}>
-          <img src="/anm.png" width='841' height='410'/>
+          <img src="/anm.png" width='1284' height='580'/>
         </Box>
       </Grid>
       <Grid item>
         <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <Link href="Home" variant="body2">
-            <Button type="submit" variant="contained" sx={{ mr: 1 }}>
+            <Button type="submit"  variant="contained"  sx={{ mt: 5, mb: 1, width: '300px', height: '50px' }}>
               เข้าสู่เว็ปไซต์
             </Button>
           </Link>
