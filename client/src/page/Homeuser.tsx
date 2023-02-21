a34c2321472fe6311a86f23c6cb6ec7444783e35
Poramee Promkeeree <poramee0013@gmail.com>
2023-02-22 03:44:51 +0700

    เพิ่มการป้องกันหากUserไม่ได้ login

--- client/src/page/Homeuser.tsx
+++ client/src/page/Homeuser.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Navbar1 from '../components/NavBar1'
+import CardLog from '../components/Body/Popupcard';
+
+const getUserData = () => {
+  const stringfiedUser = localStorage.getItem('user') || '';
+  if (stringfiedUser) {
+    return JSON.parse(stringfiedUser);
+  }
+  return false;
+};
 
 const Homeuser = () => {
+  const userData = getUserData();
+  if (!userData) {
+    return (
+      <div>
+        <CardLog/>
+      </div>
+    );
+  }
+
   return (
     <div>
-        <Navbar1/>
+      <Navbar1/>
     </div>
-  )
-}
+  );
+};
 
 export default Homeuser
