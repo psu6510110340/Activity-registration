ab6a84267de674b9201d3f20e4c08ce930d0c493
Poramee Promkeeree <poramee0013@gmail.com>
2023-02-22 01:42:11 +0700

    Reset Userเมื่อกลับมาหน้าล็อคอิน

--- client/src/page/login.tsx
+++ client/src/page/login.tsx
@@ -8,7 +8,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import FigureImage from 'react-bootstrap/FigureImage'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom';
@@ -97,6 +97,9 @@ export default function SignInSide() {
       });
     };
 
+    useEffect(() => {
+      localStorage.removeItem('user')
+  })
   
     return (
       <ThemeProvider theme={theme}>
