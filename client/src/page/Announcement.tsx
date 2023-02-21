fb6c42666d10fd45299054348f9bc940f427831f
Poramee Promkeeree <poramee0013@gmail.com>
2023-02-22 01:47:18 +0700

    แก้ไขหน้า Announcement และเพิ่มหน้าProfile Status

--- client/src/page/Announcement.tsx
+++ client/src/page/Announcement.tsx
@@ -1,33 +1,43 @@
+import React from 'react'
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider ,ThemeOptions } from '@mui/material/styles';
+import { Height } from '@mui/icons-material';
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const theme = createTheme({
   palette: {
     background: {
-      default: '#F5FFFA', // set to light blue color
+      default: '#F0FFFF', // set to light blue color
     },
   },
 });
 
 const Announcement = () => {
+  useEffect(() => {
+    localStorage.removeItem('user')
+})
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500 }}>
+      <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 600 }}>
         <Box sx={{ margin: 'auto' }}>
-          <img src="/anm.png" width='1284' height='580'/>
+          <img src="/anm.png" width='1092vh' height='533vh'/>
         </Box>
       </Grid>
       <Grid item>
-        <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' ,minHeight: 50 }}>
           <Link href="Home" variant="body2">
-            <Button type="submit"  variant="contained"  sx={{ mt: 5, mb: 1, width: '300px', height: '50px' }}>
-              เข้าสู่เว็ปไซต์
-            </Button>
+            <Button variant="contained" size="medium">
+            เข้าสู่เว็บไซต์
+          </Button>
           </Link>
         </Grid>
       </Grid>
