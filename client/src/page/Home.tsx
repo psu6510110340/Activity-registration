97d119352e1642d953f06a795fe4810c42685341
ภัคคภูมิ จินดาเดช <pakkapoom2932@gmail.com>
2023-02-22 17:41:50 +0700

    🏠 Set หน้า Home ให้เป็นระเบียบ

--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import Navbar from '../components/NavBar'
+import Navbar from '../components/NavBar';
+import FooterBar from '../components/FooterBar';
 
-const PublicHome = () => {
+const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div
+      >
+        <label
+          style={{
+            width: 'var(--dl-size-size-maxwidth)',
+            fontSize: '2rem',
+            alignSelf: 'center',
+            fontStyle: 'normal',
+            textAlign: 'center',
+            fontWeight: '900',
+            borderColor: 'var(--dl-color-gray-black)',
+            borderWidth: '1px',
+            backgroundColor: 'rgb(255, 255, 255)',
+          }}
+        >
+          <span>ข่าวสาร</span>
+          <br></br>
+        </label>
+      </div>
+      <FooterBar/>
     </div>
   )
 }
 
-export default PublicHome
+export default Home
