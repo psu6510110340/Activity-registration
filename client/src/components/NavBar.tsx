4eb1867509ad59dc85518ee1aa8708684de0344c
ภัคคภูมิ จินดาเดช <pakkapoom2932@gmail.com>
2023-02-24 00:33:47 +0700

    🍫 แก้NavBarให้อัตรส่วนคงที่ สร้างdropdownหมวดหมู่

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,128 +1,158 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider ,ThemeOptions } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import logo from '../image/WhiteLogo.png';
-import bell from '../image/WhiteBell.png';
+import { TextField } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { styled, alpha } from '@mui/material/styles';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ButtonUser from './Body/Buttonuser';
 
 const getUserData = () => {
-  const stringfiedUser = localStorage.getItem('user') || '';
+  const stringfiedUser = localStorage.getItem('usser') || '';
   if (stringfiedUser) {
     return JSON.parse(stringfiedUser);
   }
   return false;
 };
 
-const NavBar = () => {
-  const userData = getUserData();
-  return (
-    <div
-      style={{
-        width: '100%',
-        display: 'flex',
-        overflow: 'auto',
-        minHeight: '10vh',
-        alignItems: 'center',
-        borderColor: 'var(--dl-color-gray-black)',
-        borderWidth: '5px',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        backgroundColor: '#D9D9D9', //Background
+function NavBar () {
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const StyledMenu = styled((props: MenuProps) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
       }}
-    >
-      <header
-        data-role="Header"
-        style={{
-          width: '100%',
-          height: '75px', //ความสูงแถบบาร์
-          display: 'flex',
-          position: 'relative',
-          boxShadow: '5px 5px 10px 0px #545454',
-          alignItems: 'center',
-          paddingTop: 'var(--dl-space-space-twounits)',
-          paddingLeft: 'var(--dl-space-space-threeunits)',
-          paddingRight: 'var(--dl-space-space-threeunits)',
-          paddingBottom: 'var(--dl-space-space-twounits)',
-          justifyContent: 'space-between',
-          backgroundColor: '#6890F8',
-        }}
-      >
-        <span
-          style={{
-            color: 'rgb(255, 255, 255)', //ช่องว่าง
-            width: 'auto',
-            zIndex: '100',
-            fontSize: '1.7rem',
-            alignSelf: 'center',
-            textAlign: 'center',
-            fontWeight: '900',
-          }}
-        ></span>
-        <img
-          alt="WhiteLogo"
-          src={logo} //logo
-          style={{
-            width: '250px',
-            height: '100px',
-            objectFit: 'cover',
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Search" //ค้นหา
-          style={{
-            width: '531px',
-            height: '40px',
-            backgroundColor: '#d9d9d9',
-          }}
-        />
-        <img
-          alt="WhiteBell" //แจ้งเตือน
-          src={bell}
-          style={{
-            width: '40px',
-            height: '40px',
-            objectFit: 'cover',
-          }}
-        />
-        <span
-          style={{
-            color: 'rgb(255, 255, 255)', //หมวดหมู่
-            width: 'auto',
-            zIndex: '100',
-            fontSize: '1.7rem',
-            alignSelf: 'center',
-            textAlign: 'center',
-            fontWeight: '900',
-          }}
-        >
-          หมวดหมู่
-        </span>
-        <ButtonUser/>
-        <span
-          style={{
-            color: 'rgb(255, 255, 255)', //ช่องว่าง
-            width: 'auto',
-            zIndex: '100',
-            fontSize: '1.7rem',
-            alignSelf: 'center',
-            textAlign: 'center',
-            fontWeight: '900',
-          }}
-        ></span>
-        <p>
-          
-        </p>
-      </header>  
-    </div>
-  )
-}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    '& .MuiPaper-root': {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      boxShadow:
+        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+        padding: '4px 0',
+      },
+      '& .MuiMenuItem-root': {
+        '& .MuiSvgIcon-root': {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        '&:active': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  }));
+
+  const userData = getUserData();
+  
+    return (
+        <AppBar position="static" >
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <img
+                alt="Logo"                          //LOGO
+                src={logo}
+                style={{
+                  width: '250px',
+                  height: '65px',
+                  margin: '5px 25px 5px 0px'
+                }}
+              />
+             
+              <Box
+                sx={{
+                  width: '600px',
+                  height: '10%',                //Search
+                  background: '#ffffff',
+                  borderRadius: 3,
+                  margin: '10px 25px 10px 0px'
+                }}
+              >
+                <TextField fullWidth label="Search" id="Search"/>
+              </Box>
+              
+
+              <NotificationsIcon sx={{
+                  width: '40px',
+                  height: '40px',
+                  margin: '5px 25px 5px 0px',     //Bell
+                  '&:hover': {
+                    opacity: [0.9, 0.8, 0.7]      
+                  }
+                }}/>
+              
+
+              <div>
+                <Button style={{ fontWeight: '900', fontSize: '1.2rem', margin: '5px 25px 5px 0px' }}
+                  id="demo-customized-button"                             //หมวดหมู่
+                  aria-controls={open ? 'demo-customized-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                  variant="contained"
+                  disableElevation
+                  onClick={handleClick}
+                  endIcon={<KeyboardArrowDownIcon />}
+                >
+                  หมวดหมู่
+                </Button>
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <ArrowRightIcon />
+                    กิจกรรมมหาวิทยาลัย
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArrowRightIcon />
+                    กิจกรรมจิตอาสา
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+              <ButtonUser/>
+
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  }
 
 export default NavBar
