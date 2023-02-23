859aa2194f32bced5a1d92d291342cec7f9c649f
ภัคคภูมิ จินดาเดช <pakkapoom2932@gmail.com>
2023-02-24 00:35:21 +0700

    🔳 แก้ปุ่ม Login เพิ่มเอฟเฟกต์ และแก้ไลบารี

--- client/src/components/Body/Buttonuser.tsx
+++ client/src/components/Body/Buttonuser.tsx
@@ -1,21 +1,14 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import logo from "../image/logo.png";
-import { Link as NewLink } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { TextField } from "@mui/material";
 import Grid from '@mui/material/Grid';
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 const settings = ["Profile", "Logout"];
 
@@ -54,43 +47,21 @@ const ButtonUser = () => {
   if (!userData) {
     return (
       <div>
-        <button
-          style={{
-            color: "var(--dl-color-gray-black)", //login
-            width: "170px",
-            height: "50px",
-            position: "relative",
-            boxShadow: "5px 5px 10px 0px #7c7c7c",
-            textAlign: "center",
-            textTransform: "capitalize",
-          }}
-        >
-          <NewLink href="../login">
-            <h1
-              style={{
-                top: "6px",
-                left: "0px",
-                color: "rgb(53, 58, 104)",
-                right: "0px",
-                width: "auto",
-                bottom: "0px",
-                height: "38px",
-                margin: "auto",
-                position: "absolute",
-                textDecoration: "none",
-              }}
-            >
-              <span
-                style={{
-                  fontWeight: "700",
-                }}
-              >
-                Login
-              </span>
-              <br></br>
-            </h1>
-          </NewLink>
-        </button>
+        <Link to='../login'>
+          <Button sx={{
+              width: '150px',
+              height: '10%',                 //Login
+              background: '#ffffff',
+              '&:hover': {
+                background: '#ffffff',
+                opacity: [0.9, 0.8, 0.7]
+              },
+              fontSize: '1.2rem',
+              fontWeight: '700',
+              borderRadius: 3,
+              margin: '10px 0px 10px 0px'
+            }}>Login</Button>
+        </Link>
       </div>
     );
   }
