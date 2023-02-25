7a7f8fa42df07e354898ec1534ad8cb55a1dbd97
Nopphakao <6510110218@email.psu.ac.th>
2023-02-25 23:15:19 +0700

    🌍ActiviForm (ขาด import image)

--- client/src/page/ActivityForm.tsx
+++ client/src/page/ActivityForm.tsx
@@ -13,6 +13,10 @@ const ActivityForm = (): JSX.Element => {
   const [description, setDescription] = useState<string>("");
   const [startActivity, setStartActivity] = useState<string>("");
   const [user, setUser] = useState<IUser | null>(null);
+  const [EndActivity, setEndActivity ] = useState<string>("");
+  const [StartRegister, setStartRegister] = useState<string>("");
+  const [EndRegister, setEndRegister] =useState<string>("")
+  const [Number, setNumber] = useState<number>(0)
 
   useEffect(() => {
     const userData = localStorage.getItem("user");
@@ -34,6 +38,9 @@ const ActivityForm = (): JSX.Element => {
 
     // Convert StartActivity to a date
     const startDate = new Date(startActivity);
+    const EndDate = new Date(EndActivity);
+    const startRegister = new Date(StartRegister);
+    const endRegister = new Date(EndRegister);
 
     try {
       const response = await fetch("http://localhost:1337/api/activities", {
@@ -47,6 +54,10 @@ const ActivityForm = (): JSX.Element => {
             title,
             description,
             StartActivity: startDate.toISOString().substring(0, 10),
+            EndActivity: EndDate.toISOString().substring(0, 10),
+            StartRegister: startRegister.toISOString().substring(0, 10),
+            EndRegister: endRegister.toISOString().substring(0, 10),
+            Number
           },
         }),
       });
@@ -79,6 +90,35 @@ const ActivityForm = (): JSX.Element => {
           value={startActivity}
           onChange={(event) => setStartActivity(event.target.value)}
         />
+        <TextField
+          type="date"
+          variant="outlined"
+          fullWidth
+          value={EndActivity}
+          onChange={(event) => setEndActivity(event.target.value)}
+        />
+        <TextField
+          type="date"
+          variant="outlined"
+          fullWidth
+          value={StartRegister}
+          onChange={(event) => setStartRegister(event.target.value)}
+        />
+        <TextField
+          type="date"
+          variant="outlined"
+          fullWidth
+          value={EndRegister}
+          onChange={(event) => setEndRegister(event.target.value)}
+        />
+        <TextField
+          type="number"
+          variant="outlined"
+          fullWidth
+          value={Number}
+          onChange={(event) => setNumber(parseInt(event.target.value))}
+        />
+
         <Button type="submit" variant="contained" color="primary">
           Create Activity
         </Button>
