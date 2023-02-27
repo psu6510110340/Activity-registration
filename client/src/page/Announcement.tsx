9af35076ab58012bdb1edfa6730c63645f7ccb0e
นิธิกร ปานสังข์ <pansang1223@gmail.com>
2023-02-27 14:54:36 +0700

    ตกเเต่งหน้าเเรก

--- client/src/page/Announcement.tsx
+++ client/src/page/Announcement.tsx
@@ -1,47 +1,21 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider ,ThemeOptions } from '@mui/material/styles';
-import { Height } from '@mui/icons-material';
-import { useState,useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const theme = createTheme({
-  palette: {
-    background: {
-      default: '#F0FFFF', // set to light blue color
-    },
-  },
-});
+import {useEffect } from 'react';
+import './Announcement.css';
 
 const Announcement = () => {
   useEffect(() => {
     localStorage.removeItem('user')
 })
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 600 }}>
-        <Box sx={{ margin: 'auto' }}>
-          <img src="/anm.png" width='1092vh' height='533vh'/>
-        </Box>
+      <div>
+      <Grid className="grid-container" >
+          <text className='text'>TakeCamp </text>
+        <Link href="Home" variant="body2">
+          <a className='button' href='http://localhost:3000/Home'>เข้าสู่เว็บไซต์</a>
+        </Link>
       </Grid>
-      <Grid item>
-        <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' ,minHeight: 50 }}>
-          <Link href="Home" variant="body2">
-            <Button variant="contained" size="medium">
-            เข้าสู่เว็บไซต์
-          </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+      </div>
   )
 }
 
