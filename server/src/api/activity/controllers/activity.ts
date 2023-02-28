57ace7f455851e9e20d4da21188299f6308aac67
Nopphakao <6510110218@email.psu.ac.th>
2023-02-28 13:20:22 +0700

    🌍เพิ่มฟังก์ชั่นการนับคน

--- server/src/api/activity/controllers/activity.ts
+++ server/src/api/activity/controllers/activity.ts
@@ -3,5 +3,41 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { parseMultipartData } from '@strapi/utils'
 
-export default factories.createCoreController('api::activity.activity');
+export default factories.createCoreController('api::activity.activity', ({ strapi }) => ({
+    async update(ctx) {
+      const entityId = ctx.params.id;
+      let entity;
+    const activity = await strapi.entityService.findOne('api::activity.activity', entityId, {
+        populate: { owner: true },
+    });
+    if (!activity) {
+        return ctx.notFound('Not Found');
+      }
+    if (activity.owner?.id !== ctx.state.user.id) {
+        return ctx.unauthorized('You cant update this entry');
+      }
+      if (ctx.is('multipart')) {
+        const { data, files } = parseMultipartData(ctx);
+        entity = await strapi.entityService.update('api::activity.activity', entityId, { data }, { files });
+      } else {
+        entity = await strapi.entityService.update('api::activity.activity', entityId, ctx.request.body);
+      }
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitizedEntity);
+  },
+
+  async like(ctx) {
+    const entityId = ctx.params.id;
+    try {
+        let activity = await strapi.entityService.findOne('api::activity.activity', entityId)
+        activity = await strapi.entityService.update('api::activity.activity', entityId, { data: { likeCount: (activity.likeCount || 0) + 1} })
+        ctx.body = { ok: 1, likeCount: activity.likeCount };
+    } catch (err) {
+        ctx.body = err;
+    }
+},
+
+}));
+
