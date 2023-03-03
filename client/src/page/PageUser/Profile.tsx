f61e8042822b7baa5e671194d11263f368679862
นิธิกร ปานสังข์ <pansang1223@gmail.com>
2023-03-03 16:05:33 +0700

    เเก้ไขโปรไฟล์เพิ่ม

--- client/src/page/PageUser/Profile.tsx
+++ client/src/page/PageUser/Profile.tsx
@@ -1,30 +1,8 @@
 import Navbar from '../../components/Body/StatusNavbar';
-import React from 'react';
 import CardLog from '../../components/Body/Popupcard';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import FigureImage from 'react-bootstrap/FigureImage'
-import { useState,useEffect } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify'
-import { useNavigate } from 'react-router-dom';
-import { storeUser } from '../../helper';
-import logo from '../../image/logo.png';
-import backgroundImage from '../../image/PSU.jpg';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content';
-
 import './Profile.css'
-import { AppBar, Avatar, Container, Toolbar } from '@mui/material';
-import FooterBar from '../../components/FooterBar';
-import IconButton from '@mui/joy/IconButton/IconButton';
+import { AppBar,Container, Toolbar } from '@mui/material';
 
 const getUserData = () => {
   const stringfiedUser = localStorage.getItem('user') || '';
@@ -45,33 +23,35 @@ const Profile = () => {
   }
 
   return (
-    <>
-      <div>
-        <Navbar />
-        <a className='my_button1'>ตรวจเช็คสถานะ</a>
-        <a className='my_button2'>กิจกรรมที่เคยเข้าร่วม</a>
+    <><div className='backround'>
+        <div>
+          <Navbar />
+          <a className='my_button1'>ตรวจเช็คสถานะ</a>
+          <a className='my_button2'>กิจกรรมที่เคยเข้าร่วม</a>
+          <div className='Image'></div>
+          <div className='line'></div>
+          <AppBar position="static"> 
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6" //#1976D2
+              noWrap
+              component="a"
+              sx={{
+                margin: '0px 30px 0px 930px', //Contact:
+                display: { xs: 'none', md: 'flex' },
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
       </div>
-        <div className='Image'></div>
-        <AppBar position="static"> 
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6" //#1976D2
-            noWrap
-            component="a"
-            sx={{
-              margin: '0px 30px 0px 930px', //Contact:
-              display: { xs: 'none', md: 'flex' },
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    </div>
     </>
   );
 };
